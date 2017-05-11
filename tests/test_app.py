97162a3fc99aa3c5bcd17d0e8fcca58526007c97
Jan Staněk <jstanek@redhat.com>
2017-05-11 16:15:16 +0200

    Replace the app module

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,86 +1,29 @@
+"""Test argument parsing"""
+
 import os
-from unittest import mock
 
-from rpmlb.app import Application
-
-
-def test_init():
-    app = Application()
-    assert app
+from rpmlb.cli import run
 
 
-def test_run_calls_run_internally():
-    app = Application()
-    app.run_internally = mock.MagicMock(return_value=True)
-    app.run()
-    assert app.run_internally.called
+def test_parse_argv_no_options(tmpdir):
+    """Tests proper default values of the CLI"""
 
+    RECIPE_FILE = tmpdir.join('ror.yml')
+    RECIPE_NAME = 'rh-ror50'
 
-def test_run_returns_true_on_success():
-    app = Application()
-    app.run_internally = lambda *args: None
-    assert app.run()
+    # Prepare environment
+    RECIPE_FILE.write('')
 
+    # Parse the arguments
+    with tmpdir.as_cwd():
+        argv = list(map(str, [RECIPE_FILE, RECIPE_NAME]))
+        args = run.make_context('rpmlb', argv).params
 
-def test_run_returns_false_on_exception():
-    app = Application()
+    current_dir = os.path.abspath(os.getcwd())
 
-    def run_internally(*args):
-        raise RuntimeError('test')
-
-    app.run_internally = run_internally
-    assert not app.run()
-
-
-def test_parse_argv_without_option_returns_dict():
-    app = Application()
-    args_dict = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50'])
-    assert args_dict
-    assert args_dict['recipe_file'] == '/tmp/ror.yml'
-    assert args_dict['recipe_id'] == 'rh-ror50'
-    assert args_dict['build'] == 'dummy'
-    assert args_dict['download'] == 'none'
-    assert 'branch' not in args_dict
-    assert 'resume' not in args_dict
-    current_dir = os.getcwd()
-    assert args_dict['source_directory'] == current_dir
-
-
-def test_parse_argv_with_build_option_returns_dict():
-    app = Application()
-    args_dict = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50',
-                                '--build', 'mock'])
-    assert args_dict
-    assert args_dict['build'] == 'mock'
-
-
-def test_parse_argv_with_download_option_returns_dict():
-    app = Application()
-    args_dict = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50',
-                                '--download', 'rhpkg'])
-    assert args_dict
-    assert args_dict['download'] == 'rhpkg'
-
-
-def test_parse_argv_with_branch_option_returns_dict():
-    app = Application()
-    args_dict = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50',
-                                '--branch', 'rhscl-2.3-rh-ror42-rhel-7'])
-    assert args_dict
-    assert args_dict['branch'] == 'rhscl-2.3-rh-ror42-rhel-7'
-
-
-def test_parse_argv_with_source_directory_option_returns_dict():
-    app = Application()
-    args_dict = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50',
-                                '--source-directory', '/tmp/foo'])
-    assert args_dict
-    assert args_dict['source_directory'] == '/tmp/foo'
-
-
-def test_parse_argv_with_resume_option_returns_dict():
-    app = Application()
-    args_dict = app.parse_argv(['prog', '/tmp/ror.yml', 'rh-ror50',
-                                '--resume', '02'])
-    assert args_dict
-    assert args_dict['resume'] == 2
+    assert args['recipe_file'] == str(RECIPE_FILE), 'Wrong recipe file path'
+    assert args['recipe_name'] == str(RECIPE_NAME), 'Wrong recipe name'
+    assert args['build'] == 'dummy', 'Wrong builder name'
+    assert args['download'] == 'none', 'Wrong downloader name'
+    assert args['branch'] is None, 'Superfluous branch'
+    assert args['source_directory'] == current_dir
